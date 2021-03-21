1d2f3764ec6051d7301264d69b276bd78bd72f27
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-03-21 11:36:14 -0300

    Adds mux router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+)
+
+type Hello struct {
+	Msg string `json:"msg"`
+}
+
+var hello = Hello{Msg: "Hello World!"}
 
 func main() {
-	fmt.Println("Hello world!")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", HomeHandler).Methods("GET")
+	r.HandleFunc("/products", HomeHandler)
+	r.HandleFunc("/articles", HomeHandler)
+	http.ListenAndServe(":3000", r)
+
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(hello)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jotajay/go-quiz-app
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
