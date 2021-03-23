0ab876c4cf3632b857cc044184bae4bc074ce878
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-03-23 16:40:37 -0300

    creates POST /users

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"encoding/json"
@@ -12,19 +13,35 @@ type Hello struct {
 	Msg string `json:"msg"`
 }
 
+type Account struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 var hello = Hello{Msg: "Hello World!"}
 
 func main() {
 	r := mux.NewRouter()
+	port := ":3000"
 
 	r.HandleFunc("/", HomeHandler).Methods("GET")
-	r.HandleFunc("/products", HomeHandler)
+	r.HandleFunc("/users", CreateUser).Methods("POST")
 	r.HandleFunc("/articles", HomeHandler)
-	http.ListenAndServe(":3000", r)
+
+	fmt.Printf(`Server running and listening on port %v`, port)
+	http.ListenAndServe(port, r)
 
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(hello)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	fmt.Printf("params %v", params)
 }
