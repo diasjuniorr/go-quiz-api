0e18634e93e870daff2c7d3d22f19d1e39b30687
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-03-24 13:28:22 -0300

    adds POST GET /users

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,39 +9,47 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Hello struct {
-	Msg string `json:"msg"`
+type HealthCheck struct {
+	Version string `json:"version"`
+	Status  string `json:"status"`
 }
 
-type Account struct {
+type User struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-var hello = Hello{Msg: "Hello World!"}
+var healthStauts = HealthCheck{Version: "1.0", Status: "ok"}
 
 func main() {
 	r := mux.NewRouter()
 	port := ":3000"
 
-	r.HandleFunc("/", HomeHandler).Methods("GET")
+	r.HandleFunc("/", checkApiHealth).Methods("GET")
 	r.HandleFunc("/users", CreateUser).Methods("POST")
-	r.HandleFunc("/articles", HomeHandler)
+	r.HandleFunc("/users/{id}", getUser).Methods("GET")
 
 	fmt.Printf(`Server running and listening on port %v`, port)
 	http.ListenAndServe(port, r)
 
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func checkApiHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(hello)
+	json.NewEncoder(w).Encode(healthStauts)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	json.NewEncoder(w).Encode(user)
 
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	fmt.Printf("params %v", params)
+	json.NewEncoder(w).Encode(params["id"])
 }
